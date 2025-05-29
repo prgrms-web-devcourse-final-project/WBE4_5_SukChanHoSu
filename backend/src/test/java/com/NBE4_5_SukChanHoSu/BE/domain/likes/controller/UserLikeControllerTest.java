acb7b9bc4cd15da83d6e3587822942e6e3045233
wonseokyoon <01088115030a@gmail.com>
2025-05-29 19:35:04 +0900

    fix:test

--- backend/src/test/java/com/NBE4_5_SukChanHoSu/BE/domain/likes/controller/UserLikeControllerTest.java
+++ backend/src/test/java/com/NBE4_5_SukChanHoSu/BE/domain/likes/controller/UserLikeControllerTest.java
@@ -61,6 +61,9 @@ public class UserLikeControllerTest {
 
     @BeforeEach
     void setUp() {
+        clearRedisData(); // 레디스 데이터 초기화
+        ClearStream(LIKE_STREAM);   // 스트림 초기화
+        ClearStream(MATCHING_STREAM);
         objectMapper = new ObjectMapper();
         login();
     }
